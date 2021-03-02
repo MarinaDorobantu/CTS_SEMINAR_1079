8ab29baee5cba1996c1d82fd3c74ffe7f2763e02
Marina Dorobantu <dorobantu_marina_andreea99@yahoo.com>
2021-03-02 14:52:36 +0200

    classes!

--- Seminar_1/src/main/Main.java
+++ Seminar_1/src/main/Main.java
@@ -1,10 +1,16 @@
 package main;
 
+import classes.Zoo;
+import classes.Zookeeper;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Seminar 1");
+//		System.out.println("Seminar 1");
+		Zookeeper keeper = new Zookeeper("Dani");
+		Zoo zoo = new Zoo();
+		zoo.setZookeeper(keeper);
 	}
 
 }
